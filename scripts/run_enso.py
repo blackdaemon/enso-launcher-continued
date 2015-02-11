af6fc0abb246dcb5cd104973854c0827831e134f
Pavel Vitis (blackdaemon) <pavelvitis@gmail.com>
2015-02-12 00:13:58 +0100

    Improved startup script Added command-line parameters

--- scripts/run_enso.py
+++ scripts/run_enso.py
@@ -1,22 +1,200 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import os
 import logging
+import os
+import sys
+import threading
 
-import enso
+import enso.config
+from enso.messages import displayMessage
+from optparse import OptionParser
+
+
+ENSO_DIR = os.path.realpath(os.path.join(os.path.dirname(sys.argv[0]), ".."))
+
+options = None
+
+
+def change_color_scheme(color):
+    """Change Enso color scheme"""
+    if color not in enso.config.COLOR_SCHEMES:
+        print "Unknown color scheme '%s'. Leaving defaults." % color
+        return
+    from enso.quasimode import layout
+    scheme = enso.config.COLOR_SCHEMES[color]
+    layout.WHITE = scheme[0]
+    layout.DESIGNER_GREEN = scheme[1]
+    layout.DARK_GREEN = scheme[2]
+    layout.BLACK = scheme[3]
+    layout.DESCRIPTION_BACKGROUND_COLOR = layout.DESIGNER_GREEN + "cc"
+    layout.MAIN_BACKGROUND_COLOR = layout.BLACK + "d8"
+
+
+def process_options(argv):
+    version = '1.0'
+    usageStr = "%prog [options]\n\n"
+    parser = OptionParser(usage=usageStr, version="%prog " + version)
+
+    parser.add_option("-l", "--log-level", action="store", dest="loglevel", default="ERROR", help="logging level (CRITICAL, ERROR, INFO, WARNING, DEBUG)")
+    parser.add_option("-n", "--no-splash", action="store_false", dest="show_splash", default=True, help="Do not show splash window")
+    parser.add_option("-c", "--no-console", action="store_false", dest="show_console", default=True, help="Hide console window")
+    parser.add_option("-q", "--quiet", action="store_true", dest="quiet", default=False, help="No information windows are shown on startup/shutdown")
+    parser.add_option("-k", "--hotkey", action="store", dest="hotkey", default="default", help="Hotkey used to invoke Enso. Possible values are: CAPITAL, LSHIFT, RSHIFT, LCONTROL, RCONTROL, LWIN, RWIN")
+    parser.add_option("", "--ignore-ensorc", action="store_true", dest="ignore_ensorc", default=False, help="Ignore .ensorc file")
+
+    # Hidden options useful for development
+    parser.add_option("", "--commands-dir", action="store", dest="commands_dir", default="default", help="Used to override name of the subdirectory in user home directory that stores custom commands (used for development)")
+    parser.add_option("", "--color-scheme", action="store", dest="color_scheme", default="default", help="Used to override default color scheme (used for development)")
+
+    if sys.platform.startswith("win"):
+        # Add tray-icon support for win32 platform
+        parser.add_option("-t", "--no-tray", action="store_false", dest="show_tray_icon", default=True, help="Hide tray icon")
+
+    opts, args = parser.parse_args(argv)
+    return opts, args
+
+
+def main(argv = None):
+    global options
+
+    opts, args = process_options(argv[1:])
+    options = opts
+
+    enso.config.CMDLINE_OPTIONS = options
+
+    logformat = "%(levelname)s\t%(asctime)s %(pathname)s[%(funcName)s:%(lineno)d]: %(message)s"
+    loglevel = {
+        'CRITICAL' : logging.CRITICAL,
+        'ERROR' : logging.ERROR,
+        'INFO' : logging.INFO,
+        'WARNING' : logging.WARNING,
+        'DEBUG' : logging.DEBUG
+        }.get(opts.loglevel, logging.NOTSET)
+
+    if opts.show_console:
+        print "Showing console"
+        logging.basicConfig(level=loglevel, format=logformat)
+    else:
+        print "Hiding console"
+        print "Logging into '%s'" % os.path.join(ENSO_DIR, "enso.log")
+        sys.stdout = open("stdout.log", "w", 0) #NullDevice()
+        sys.stderr = open("stderr.log", "w", 0) #NullDevice()
+        logging.basicConfig(
+            filename=os.path.join(ENSO_DIR, "enso.log"),
+            level=loglevel,
+            format=logformat)
+
+    if loglevel == logging.DEBUG:
+        logging.debug("default options set:" + repr(opts))
+        logging.debug("command-line args:" + repr(args))
+
+    if not opts.ignore_ensorc:
+        ensorc_path = os.path.expanduser(os.path.join("~",".ensorc"))
+        if (not os.path.isfile(ensorc_path) and sys.platform.startswith("win")
+            and os.path.isfile(ensorc_path+".lnk")):
+            # Extract real .ensorc path from .ensorc.lnk file on Windows platform
+            try:
+                import pythoncom
+                from win32com.shell import shell, shellcon
+                link = pythoncom.CoCreateInstance(
+                    shell.CLSID_ShellLink,
+                    None,
+                    pythoncom.CLSCTX_INPROC_SERVER,
+                    shell.IID_IShellLink
+                )
+                link.QueryInterface(pythoncom.IID_IPersistFile).Load(ensorc_path+".lnk")
+                path = link.GetPath(shell.SLGP_UNCPRIORITY)
+                if path and path[0]:
+                    ensorc_path = path[0]
+            except Exception, e:
+                logging.error("Error parsing .ensorc.lnk file: %s", e)
+
+        if os.path.isfile(ensorc_path):
+            logging.info( "Loading '%s'." % ensorc_path )
+            contents = open( ensorc_path, "r" ).read()
+            compiledContents = compile( contents + "\n", ensorc_path, "exec" )
+            exec compiledContents in {}, {}
+        else:
+            logging.warning(".ensorc file can't be read!")
+    else:
+        logging.info("Ignoring your .ensorc startup script")
+
+    if opts.hotkey in ("default", "CAPITAL", "LSHIFT", "RSHIFT", "LCONTROL", "RCONTROL", "LWIN", "RWIN"):
+        if opts.hotkey != "default":
+            #contents += "enso.config.QUASIMODE_START_KEY = \"KEYCODE_%s\"\n" % opts.hotkey
+            enso.config.QUASIMODE_START_KEY = "KEYCODE_%s" % opts.hotkey
+            logging.info("Enso hotkey has been set to %s" % opts.hotkey)
+    else:
+        logging.error("Invalid hotkey spec: %s" % opts.hotkey)
+
+
+    if not opts.quiet and opts.show_splash:
+        displayMessage("<p><command>Enso</command> is starting...</p>")
+
+    if sys.platform.startswith("win"):
+        # Add tray-icon support for win32 platform
+        if opts.show_tray_icon:
+            # tray-icon code must be run in separate thread otherwise it blocks
+            # current thread (using PumpMessages() )
+            try:
+                import enso.platform.win32.taskbar as taskbar
+                threading.Thread(target = taskbar.systray, args = (enso.config,)).start()
+            except Exception, e:
+                logging.error("Error initializing taskbar systray icon: %s", e)
+
+    if opts.commands_dir != "default":
+        logging.info("Default commands directory changed to \"%s\"" % opts.commands_dir)
+        enso.config.SCRIPTS_FOLDER_NAME = opts.commands_dir
+
+    if opts.color_scheme != "default":
+        logging.info("Changing color scheme to %s" % opts.color_scheme)
+        change_color_scheme(opts.color_scheme)
+
+    """
+    import pysage
+    
+    class EnsoRemoteMessage(pysage.Message):
+        properties = ['amount']
+        types = ['i']
+        packet_type = 101
+    
+    class EnsoRemoteConsumer(pysage.Actor):
+        subscriptions = ['EnsoRemoteMessage']
+        def handle_EnsoRemoteMessage(self, msg):
+            print 'Yummy! I had %d pancakes!' % (msg.get_property('amount'))
+
+    class Chef(pysage.Actor):
+        def __init__(self):
+            self.last_sent = time.time()
+        
+        def update(self):
+            '''every 2 seconds, this chef makes a random amount of pancakes'''
+            if time.time() - self.last_sent > 2.0:
+                mgr.queue_message_to_group(mgr.PYSAGE_MAIN_GROUP, FoodAvailableMessage(amount=random.randint(0,10)))
+                self.last_sent = time.time()
+
+    mgr = pysage.ActorManager.get_singleton()
+    mgr.register_actor(EnsoRemoteConsumer())
+    """
+    
+    try:
+        # Use optimization if available
+        import psyco
+        logging.info("Using Psyco optimization")
+        psyco.profile()
+        #psyco.log()
+    except:
+       pass
+
+    # Execute main Enso loop
+    enso.run()
+
+    return 0
+
 
 if __name__ == "__main__":
-    logging.basicConfig( level=logging.INFO )
+    sys.exit(main(sys.argv))
 
-    ensorc_path = os.path.expanduser(os.path.join("~", ".ensorc"))
-    if os.path.isfile(ensorc_path):
-        logging.info( "Loading '%s'." % ensorc_path )
-        contents = open( ensorc_path, "r" ).read()
-        compiledContents = compile( contents + "\n", ensorc_path, "exec" )
-        exec compiledContents in {}, {}
-    else:
-        logging.warning(".ensorc file can't be read!")
-
-    enso.run()
 
 # vim:set tabstop=4 shiftwidth=4 expandtab:
