4bdd11b64735b1aceb86493de9f4dc97049ac5d3
Pavel Vitis (blackdaemon) <pavelvitis@gmail.com>
2015-02-12 00:12:35 +0100

    Added Windows binaries

--- enso/platform/win32/__init__.py
+++ enso/platform/win32/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2008, Humanized, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
@@ -14,7 +14,7 @@
 #    3. Neither the name of Enso nor the names of its contributors may
 #       be used to endorse or promote products derived from this
 #       software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY Humanized, Inc. ``AS IS'' AND ANY
 # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -41,13 +41,47 @@ if not sys.platform.startswith("win"):
 # applications can get in the way
 os.environ["PATH"] = "%s;%s" % (os.path.abspath( __path__[0] ), os.environ["PATH"])
 
+os_path_expanduser = os.path.expanduser
+
+def _expanduser( path ):
+    """
+    Expand ~ and ~user constructs.
+    """
+    # Rewrite logic from ntpath.py for Windows platform
+    #assert isinstance(path, unicode), "'path' parameter must be unicode string"
+    if not isinstance(path, unicode):
+        # Use original function for non-unicode path
+        return os_path_expanduser(path)
+
+    if path[:1] != '~':
+        return path
+    i, n = 1, len(path)
+    while i < n and path[i] not in '/\\':
+        i = i + 1
+
+    # Microsoft approved way of getting the personal directory path:
+    # Use this as it always returns unicode string in contrary to standard
+    # method using environ['USERPRPFILE'] that contains national-encoded string
+    from win32com.shell import shell, shellcon
+    userhome = shell.SHGetFolderPath(0, shellcon.CSIDL_PROFILE, 0, 0)
+
+    if i != 1: #~user
+        userhome = os.path.join(os.path.dirname(userhome), path[1:i])
+
+    return userhome + path[i:]
+
+# Hack standard os.path.expanduser with fixed one correctly handling national
+# characters in username.
+os.path.expanduser = _expanduser
+
+
 def get_script_folder_name():
-  """Returns the folder where Enso commands are found. This function
-     is responsible for ensuring that this folder exists: it must not
-     return a path that is not present! It is expected to place this
-     folder in some platform-specific logical location."""
-  raise NotImplementedError("This platform does not define a "
-      "scripts folder (this needs fixing)")
+    """Returns the folder where Enso commands are found. This function
+    is responsible for ensuring that this folder exists: it must not
+    return a path that is not present! It is expected to place this
+    folder in some platform-specific logical location."""
+    raise NotImplementedError("This platform does not define a "
+        "scripts folder (this needs fixing)")
 
 # Import and return the Win32 implementation of the requested interface.
 
@@ -71,9 +105,16 @@ def provideInterface( name ):
     elif name == "selection":
         import enso.platform.win32.selection
         return enso.platform.win32.selection
+    elif name == "system":
+        import enso.platform.win32.system
+        return enso.platform.win32.system
     elif name == "scripts_folder":
         from enso.platform.win32.scriptfolder import get_script_folder_name
         return get_script_folder_name
+    elif name == "directory_watcher":
+        import enso.platform.win32.dirwatcher
+        return enso.platform.win32.dirwatcher
     else:
         return None
 
+# vim:set ff=unix tabstop=4 shiftwidth=4 expandtab:
