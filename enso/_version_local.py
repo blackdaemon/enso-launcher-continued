057ae663bda994bebab1bd0577ba42f8832c69e0
Pavel Vitis "blackdaemon" <pavelvitis@gmail.com>
2017-02-17 19:28:32 +0100

    New commands with search suggestions support" wikipedia, ebay, urbandictionary.
    - Renamed google.py to websearch.py and made it easier to implement new
    search engines with search suggestions support (plan is to make it vene
    easier to configure in the config file)
    - Lowered the suggestion query poll time from 300ms to 100ms
    - Search-suggestions are now turned on by default

---
+++ enso/_version_local.py
@@ -0,0 +1,3 @@
+# This file is originally generated from Git information by running 'setup.py
+# version'. Distribution tarballs contain a pre-generated copy of this file.
+VERSION = '1.2-67-gfd09873-dirty'
---
+++ enso/_version_remote.py
@@ -0,0 +1,3 @@
+# This file is originally generated from Git information by running 'setup.py
+# version'. Distribution tarballs contain a pre-generated copy of this file.
+VERSION = 'origin/master 1.2-61 [2015-12-22 02:27:59 +0100]'
