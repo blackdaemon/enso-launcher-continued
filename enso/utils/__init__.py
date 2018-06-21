1fa91278c353ce4f2786fb1908978fed3a2572b1
Pavel Vitis (blackdaemon) <pavelvitis@gmail.com>
2018-06-21 23:44:39 +0200

    New utils for calling function once

--- enso/utils/__init__.py
+++ enso/utils/__init__.py
@@ -1,24 +1,60 @@
 import traceback
+import inspect
 
 
-def do_once(func, *args, **kwargs):
-    """ Execute callback function just once """
+def __do_once(ignore_args, func, *args, **kwargs):
+    """ Execute the function just once """
     global __DO_ONCE_CACHE
 
     stack = traceback.extract_stack()
     stack.pop()
+    stack.pop()
     code_location = "|".join(str(i) for i in stack.pop()[:-1])
 
+    cache_id = "{}|{}|{}".format(
+        code_location,
+        func,
+        "|".join(str(arg) for arg in args) if not ignore_args else "",
+        "|".join(kwargs.values()) if not ignore_args else "",
+    )
+
     try:
-        if code_location in __DO_ONCE_CACHE:
+        if cache_id in __DO_ONCE_CACHE:
             return
     except NameError:
         __DO_ONCE_CACHE = {}
 
     try:
-        func(*args, **kwargs)
+        return func(*args, **kwargs)
     finally:
-        __DO_ONCE_CACHE[code_location] = 1
+        __DO_ONCE_CACHE[cache_id] = 1
 
 
-call_once = do_once
+def call_once(func):
+    """ Function decorator. Execute the function just once, 
+    no matter the arguments values 
+    """
+    def func_wrapper(*args, **kwargs):
+        return __do_once(True, func, *args, **kwargs)
+    return func_wrapper
+
+
+def do_once(func, *args, **kwargs):
+    """ Execute the function just once, no matter the arguments values """
+    return __do_once(True, func, *args, **kwargs)
+
+
+def call_once_for_given_args(func):
+    """ Function decorator. Execute the function just once (with given argument values).
+    Using the function with different argument values will execute it again.
+    """
+    def func_wrapper(*args, **kwargs):
+        return __do_once(False, func, *args, **kwargs)
+    return func_wrapper
+
+
+def do_once_for_given_args(func, *args, **kwargs):
+    """ Execute the function just once (with given argument values)
+    Using the function with different argument values will execute it again.
+    """
+    return __do_once(False, func, *args, **kwargs)
