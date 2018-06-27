f11a4aa6f3e7fede31065e52538f5edc59530501
Pavel Vitis (blackdaemon) <pavelvitis@gmail.com>
2018-06-28 00:29:54 +0200

    Version file tracking

--- enso/_version_remote.py
+++ enso/_version_remote.py
@@ -1,3 +1,3 @@
 # This file is originally generated from Git information by running version.py.
 # Distribution package contains a pre-generated copy of this file.
-VERSION = 'origin/master 1.2-112 [2017-03-03 21:04:23 +0100]'
+VERSION = 'f9a65f5 [2018-06-27 20:15:17 +0200]'
