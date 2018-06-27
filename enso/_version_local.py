575b4f499e87c37c89285a86889335587e981ed7
Pavel Vitis (blackdaemon) <pavelvitis@gmail.com>
2018-06-28 00:34:21 +0200

    Untrack local version

--- enso/_version_local.py
+++
@@ -1,3 +0,0 @@
-# This file is originally generated from Git information by running version.py.
-# Distribution package contains a pre-generated copy of this file.
-VERSION = '1.2-118-gbb2a43d-dirty'
