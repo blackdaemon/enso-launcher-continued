e402e3b8b4f4061caf4af02f23b69563b16f8ca0
Pavel Vitis (blackdaemon) <pavelvitis@gmail.com>
2018-07-02 23:17:38 +0200

    Update version

--- enso/_version_remote.py
+++ enso/_version_remote.py
@@ -1,3 +1,3 @@
 # This file is originally generated from Git information by running version.py.
 # Distribution package contains a pre-generated copy of this file.
-VERSION = 'f9a65f5 [2018-06-27 20:15:17 +0200]'
+VERSION = '201fe33 [2018-06-28 00:53:29 +0200]'
